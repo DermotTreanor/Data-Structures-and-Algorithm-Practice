87e4d3105b3d8332733196e1431f35ac16286c35
Dermot Treanor <dermot.treanor1@gmail.com>
2024-01-30 19:22:15 +0000

    Added the outline for the selection sort, including copying over the array and shuffle function from the bubble sort file

--- selection_sort/go_selection_sort.go
+++ selection_sort/go_selection_sort.go
@@ -2,9 +2,39 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+var orderedArray []int = []int{2, 4, 6, 8, 10, 12, 24, 36, 64, 128, 256, 512, 1024, 2048}
+
+func shuffle(array []int)(randSlice []int){
+	randSlice = make([]int, len(array))
+	randomGenerator := rand.New(rand.NewSource(time.Now().Unix()))
+	var randomIndices []int = randomGenerator.Perm(len(array))
+	for i, v := range randomIndices{
+		randSlice[i] = array[v]
+	}
+	return randSlice
+}
+
+func selectionSort(array []int){
+
+	var minimumItemIndex int = 0
+
+	for i, v := range array{
+		if v < array[minimumItemIndex] && i != minimumItemIndex{
+			minimumItemIndex = i
+		}
+	}
+		
+
+}
+
 func main(){
-	fmt.Println("hello")
+	var exampleArray []int = shuffle(orderedArray)
+	fmt.Println(exampleArray)
+	selectionSort(exampleArray)
+	fmt.Println(exampleArray)
 
 }
