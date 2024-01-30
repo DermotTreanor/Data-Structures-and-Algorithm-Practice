7b50beff0529270572cbe6a9a2ffea3ad432c41c
Dermot Treanor <dermot.treanor1@gmail.com>
2024-01-30 20:52:12 +0000

    Have the selection sort mostly complete. But will need to consider a couple of slight tweeks

--- selection_sort/go_selection_sort.go
+++ selection_sort/go_selection_sort.go
@@ -7,7 +7,6 @@ import (
 )
 
 var orderedArray []int = []int{2, 4, 6, 8, 10, 12, 24, 36, 64, 128, 256, 512, 1024, 2048}
-
 func shuffle(array []int)(randSlice []int){
 	randSlice = make([]int, len(array))
 	randomGenerator := rand.New(rand.NewSource(time.Now().Unix()))
@@ -19,16 +18,25 @@ func shuffle(array []int)(randSlice []int){
 }
 
 func selectionSort(array []int){
-
+	/*
+	In selection sort we run through an array and keep track of the lowest value's index 
+	(updating it if we find a value that's lower).
+	At the end of the run we will swap that index's value to the beginning of the array. 
+	We then run through again, but this time we start from the second position (since the first index now has the minimum).
+	We end up placing the second lowest item there. This pattern is continued until are starting position is the last item
+	in the array.
+	*/
 	var minimumItemIndex int = 0
-
-	for i, v := range array{
-		if v < array[minimumItemIndex] && i != minimumItemIndex{
-			minimumItemIndex = i
+	var currentStart int = 0
+	for currentStart != len(array) - 1{
+		for i := currentStart; i < len(array); i++{
+			if array[i] < array[minimumItemIndex]{
+				minimumItemIndex = i
+			}
 		}
+		array[currentStart], array[minimumItemIndex] = array[minimumItemIndex], array[currentStart]
+		currentStart++
 	}
-		
-
 }
 
 func main(){
@@ -36,5 +44,4 @@ func main(){
 	fmt.Println(exampleArray)
 	selectionSort(exampleArray)
 	fmt.Println(exampleArray)
-
 }
