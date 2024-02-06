11f93deafc57c70fdf98cfc1896b9412f4cbc87b
Dermot Treanor <dermot.treanor1@gmail.com>
2024-02-06 12:17:42 +0000

    Have added most of the surrounding code for the go implementation of the insertion_sort. Have the insertion_sort function but it's not working yet

--- insertion_sort/go_insertion_sort.go
+++ insertion_sort/go_insertion_sort.go
@@ -1,9 +1,49 @@
 package main
-import(
+
+import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-func main(){
-	fmt.Println("this will be where we do the insertion sort!!!!")
+var exampleArray []int = []int{2, 4, 6, 8, 10, 12, 24, 36, 64, 128, 512, 1024, 2048}
+
+func shuffle(array []int) (shuffledArray []int) {
+	shuffledArray = make([]int, len(array))
+	randomGenerator := rand.New(rand.NewSource(time.Now().Unix()))
+	shuffledIndices := randomGenerator.Perm(len(array))
+	for i := 0; i < len(array); i++ {
+		shuffledArray[i] = array[shuffledIndices[i]]
+	}
+
+	return shuffledArray
 }
 
+func InsertionSort(array []int) {
+
+	var gapIndex int = 1
+	var currentValue int
+	for gapIndex < len(array)-1 {
+		currentValue = array[gapIndex]
+		for i := gapIndex - 1; i >= 0; i-- {
+			if array[i] > array[gapIndex]{
+				array[gapIndex] = array[i]
+				gapIndex = i
+			}else{
+				array[gapIndex] = currentValue
+				break
+			}
+		}
+		gapIndex++
+
+	}
+
+}
+
+func main() {
+	// fmt.Println("Here is the ordered array: ", exampleArray)
+	exampleArray = shuffle(exampleArray)
+	fmt.Println("Here is the shuffled array: ", exampleArray)
+	InsertionSort(exampleArray)
+	fmt.Println("This is the new, sorted array: ", exampleArray)
+}
